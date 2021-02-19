2ed24fdd1ad365916ee9b149ce7099a39262f012
Jordi Hernandez <j.hernandez2@sapalomera.cat>
2021-02-19 16:22:42 +0100

    class attributes as instances of other class

--- src/models/InvoiceDetail.java
+++ src/models/InvoiceDetail.java
@@ -1,11 +1,15 @@
 package models;
 
+/* Jordi Hernandez i Gerard Lopez */
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 
-/* Jordi Hernandez i Gerard Lopez */
+import dao.ArticleDAO;
+import dao.InvoiceDAO;
+
 
 public class InvoiceDetail {
 
@@ -13,20 +17,20 @@ public class InvoiceDetail {
     private int line_number;
     private int total_articles;
     private float line_price;
-    private int article_id;
-    private int invoice_id;
-    
+    private Article article;
+    private Invoice invoice;
+      
     public InvoiceDetail() {
     	
     }
 
-    public InvoiceDetail(int id, int line_number, int total_articles, float line_price, int article_id, int invoice_id) {
+    public InvoiceDetail(int id, int line_number, int total_articles, float line_price, Article article, Invoice invoice) {
         this.id = id;
         this.line_number = line_number;
         this.total_articles = total_articles;
         this.line_price = line_price;
-        this.article_id = article_id;
-        this.invoice_id = invoice_id;
+        this.article = article;
+        this.invoice = invoice;
     }
     
     public InvoiceDetail(ResultSet resultSet) throws SQLException {
@@ -34,17 +38,26 @@ public class InvoiceDetail {
     	this.setLine_number(resultSet.getInt("line_number"));
     	this.setTotal_articles(resultSet.getInt("total_articles"));
     	this.setLine_price(resultSet.getFloat("line_price"));
-    	this.setArticle_id(resultSet.getInt("article_id"));
-    	this.setInvoice_id(resultSet.getInt("invoice_id"));
+
+    	int article_id = resultSet.getInt("article_id");
+        this.article = ArticleDAO.selectById(article_id);
+
+    	int invoice_id = resultSet.getInt("invoice_id");
+        this.invoice = InvoiceDAO.selectById(invoice_id);
+
     }
     
-    public InvoiceDetail(HttpServletRequest request) {
+    public InvoiceDetail(HttpServletRequest request) throws SQLException {
     	this.setId(Integer.parseInt(request.getParameter("id")));
     	this.setLine_number(Integer.parseInt(request.getParameter("line_number")));
     	this.setTotal_articles(Integer.parseInt(request.getParameter("total_articles")));
     	this.setLine_price(Float.parseFloat(request.getParameter("line_price")));
-    	this.setArticle_id(Integer.parseInt(request.getParameter("article_id")));
-    	this.setInvoice_id(Integer.parseInt(request.getParameter("invoice_id")));
+    	
+    	int article_id = Integer.parseInt(request.getParameter("article_id"));
+    	this.article = ArticleDAO.selectById(article_id);
+    	
+    	int invoice_id = Integer.parseInt(request.getParameter("invoice_id"));
+    	this.invoice = InvoiceDAO.selectById(invoice_id);
     }
     
 
@@ -80,19 +93,19 @@ public class InvoiceDetail {
         this.line_price = line_price;
     }
 
-    public int getArticle_id() {
-        return article_id;
+    public Article getArticle() {
+        return article;
     }
 
-    public void setArticle_id(int article_id) {
-        this.article_id = article_id;
+    public void setArticle(Article article) {
+        this.article = article;
     }
 
-    public int getInvoice_id() {
-        return invoice_id;
+    public Invoice getInvoice() {
+        return invoice;
     }
 
-    public void setInvoice_id(int invoice_id) {
-        this.invoice_id = invoice_id;
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
     }
 }
