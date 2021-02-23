12150601d72d4d3584f74cc7b95aafd7eef71241
Gerard López <46751836+Gerard1999@users.noreply.github.com>
2021-02-23 13:15:12 +0100

    AddClass Error if it's also 0

--- WebContent/js/validations.js
+++ WebContent/js/validations.js
@@ -25,7 +25,7 @@ function validateForm() {
 
 
 function validateEmpties(field) {
-    if ($(field).text() == "") {
+    if ($(field).text() == "" || $(field).text() == "0") {
         $(field).addClass("error");
         return false;
     } else {
